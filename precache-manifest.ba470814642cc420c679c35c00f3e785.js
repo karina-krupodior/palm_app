fbb8b7a74903c6227b98db1bcab0fa444f910540
Karina <karinakrupodior@gmail.com>
2021-01-06 21:58:29 +0200

    init commit

---
+++ precache-manifest.ba470814642cc420c679c35c00f3e785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5489f9b5907dcb75546d8a9388c6ed9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "880a19891e2a6ebec9d4",
+    "url": "/static/css/main.ce306eab.chunk.css"
+  },
+  {
+    "revision": "03a0ae02405eb4870699",
+    "url": "/static/js/2.5576acea.chunk.js"
+  },
+  {
+    "revision": "48508d2b3819283ab9b08cee278d8457",
+    "url": "/static/js/2.5576acea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880a19891e2a6ebec9d4",
+    "url": "/static/js/main.615c2475.chunk.js"
+  },
+  {
+    "revision": "73188c7a3dfcd97236a2",
+    "url": "/static/js/runtime-main.abfc8246.js"
+  }
+]);
